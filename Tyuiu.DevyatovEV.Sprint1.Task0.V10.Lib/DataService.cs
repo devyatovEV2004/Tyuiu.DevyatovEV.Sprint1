bccd572a403ebb0b7d6f2d3d873359f4ec0b15d9
Egor Devyatov <Tyuiu.DevtyatovEV@outlook.com>
2025-09-29 23:51:19 +0500

    Add project files.

---
+++ Tyuiu.DevyatovEV.Sprint1.Task0.V10.Lib/DataService.cs
@@ -0,0 +1,12 @@
+﻿using tyuiu.cources.programming.interfaces;
+using tyuiu.cources.programming.interfaces.Sprint1;
+namespace Tyuiu.DevyatovEV.Sprint1.Task0.V10.Lib
+{
+    public class DataService : ISprint1Task0V10
+    {
+        public double Calculate()
+        {
+            return (3 / 6 - 4) * 3;
+        }
+    }
+}
---
+++ Tyuiu.DevyatovEV.Sprint1.Task0.V10.Test/DataServiceTest.cs
@@ -0,0 +1,15 @@
+﻿using Tyuiu.DevyatovEV.Sprint1.Task0.V10.Lib;
+namespace Tyuiu.DevyatovEV.Sprint1.Task0.V10.test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            var res = ds.Calculate();
+            Assert.AreEqual(-12, res);
+        }
+    }
+}
